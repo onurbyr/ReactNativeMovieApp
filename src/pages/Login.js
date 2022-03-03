e68cd72ed9b61b8735872738055badbd6252b758
onurbyr <byronurr@gmail.com>
2022-03-03 04:06:47 +0300

    Login 3

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,15 +6,78 @@ import {
   TouchableOpacity,
   KeyboardAvoidingView,
   Image,
+  ToastAndroid,
 } from 'react-native';
 import React, {useState} from 'react';
 import BackButton from '../components/BackButton';
+import {api, apiKey} from '../../services/api/api';
 
 const Login = () => {
   const [userName, onChangeUserName] = useState('');
   const [password, onChangePassword] = useState('');
+
+  const Login = async () => {
+    if (userName && password) {
+      let requestToken = '';
+      let sessionId = '';
+      try {
+        const result = await api.get('/authentication/token/new', {
+          params: {
+            api_key: apiKey.API_KEY,
+          },
+        });
+        requestToken = result.data.request_token;
+      } catch (err) {
+        console.log(err);
+      }
+      try {
+        const result = await api.post(
+          '/authentication/token/validate_with_login',
+          {
+            username: userName,
+            password: password,
+            request_token: requestToken,
+          },
+          {
+            params: {
+              api_key: apiKey.API_KEY,
+            },
+          },
+        );
+        requestToken = result.data.request_token;
+      } catch (err) {
+        //console.log(err);
+        err.response.status == 401 &&
+          ToastAndroid.show('Invalid Username or Password', ToastAndroid.SHORT);
+      }
+      try {
+        const result = await api.post(
+          '/authentication/session/new',
+          {
+            request_token: requestToken,
+          },
+          {
+            params: {
+              api_key: apiKey.API_KEY,
+            },
+          },
+        );
+        if (result.data.success == true) {
+          ToastAndroid.show('Successful Login', ToastAndroid.SHORT);
+          sessionId = result.data.session_id;
+        }
+      } catch (err) {
+        //console.log(err);
+      }
+    } else {
+      ToastAndroid.show(
+        'Please enter your username and password',
+        ToastAndroid.SHORT,
+      );
+    }
+  };
+
   return (
-    // <View style={styles.container}>
     <KeyboardAvoidingView style={styles.container}>
       <BackButton style={{position: 'absolute', top: 20}} />
       <Image style={styles.image} source={require('../images/tmdb.png')} />
@@ -39,7 +102,9 @@ const Login = () => {
         <TouchableOpacity>
           <Text style={styles.forgetPassword}>Forget Password?</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.signInButton}>
+        <TouchableOpacity
+          style={styles.signInButton}
+          onPress={Login}>
           <Text style={styles.signInButtonText}>Sign In</Text>
         </TouchableOpacity>
         <TouchableOpacity>
