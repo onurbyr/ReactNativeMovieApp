c8b086495903c39b1dd5df62ffde3a19eecb625b
onurbyr <byronurr@gmail.com>
2022-05-16 23:52:13 +0300

    Profile Favorites 2

---
+++ src/components/Collapse.js
@@ -0,0 +1,51 @@
+import React, {useState} from 'react';
+import {Text, View, StyleSheet, Animated, TouchableOpacity} from 'react-native';
+import BoldText from './BoldText';
+import DefaultText from './DefaultText';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+const Collapse = props => {
+  const [animatedHeight] = useState(new Animated.Value(50));
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleDropdown = () => {
+    if (expanded == true) {
+      // collapse dropdown
+      Animated.timing(animatedHeight, {
+        toValue: 50,
+        duration: 200,
+        useNativeDriver: false,
+      }).start();
+    } else {
+      // expand dropdown
+      Animated.timing(animatedHeight, {
+        toValue: 250,
+        duration: 200,
+        useNativeDriver: false,
+      }).start();
+    }
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Animated.View style={[styles.container, {height: animatedHeight}]}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+        <BoldText>Sort By</BoldText>
+        <TouchableOpacity
+          onPress={() => toggleDropdown()}
+          style={styles.toggleBtn}>
+          <MaterialIcons name="sort" color={'white'} size={24} />
+        </TouchableOpacity>
+      </View>
+      {expanded == true ? props.children : null}
+    </Animated.View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+});
+
+export default Collapse;
--- src/pages/Profile/Favorites.js
+++ src/pages/Profile/Favorites.js
@@ -5,8 +5,9 @@ import {
   ActivityIndicator,
   FlatList,
   Image,
+  TouchableOpacity,
 } from 'react-native';
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {apiv4Authorized, apiImgUrl} from '../../../services/api/api';
 import usePrevious from '../../hooks/usePrevious';
@@ -15,17 +16,26 @@ import RenderFooter from '../../components/RenderFooter';
 import BoldText from '../../components/BoldText';
 import DefaultText from '../../components/DefaultText';
 import Stars from '../../components/Stars/Stars';
+import Collapse from '../../components/Collapse';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
-const Favorites = ({route}) => {
+const Favorites = () => {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
   const [page, setPage] = useState(1);
   const prevPage = usePrevious(page);
   const [isExtraLoading, setIsExtraLoading] = useState(true);
+  const [sort, setSort] = useState('created_at.asc');
+  const prevSortRef = useRef();
 
   useEffect(() => {
     getItems();
-  }, [page]);
+  }, [page, sort]);
+
+  useEffect(() => {
+    //assign the ref's current value to the count Hook
+    prevSortRef.current = sort;
+  }, [sort]); //run this code when the value of count changes
 
   const getItems = async () => {
     const apiv4 = await apiv4Authorized();
@@ -37,6 +47,7 @@ const Favorites = ({route}) => {
           {
             params: {
               page,
+              sort_by: sort,
             },
           },
         );
@@ -111,17 +122,105 @@ const Favorites = ({route}) => {
           <ActivityIndicator />
         </View>
       ) : (
-        <FlatList
-          data={data}
-          style={{marginBottom: 50}}
-          // onEndReached={() => {
-          //   setPage(page + 1);
-          //   setIsExtraLoading(true);
-          // }}
-          keyExtractor={({id}) => id}
-          // ListFooterComponent={RenderFooter(isExtraLoading)}
-          renderItem={({item}) => renderItem(item)}
-        />
+        <View>
+          <Collapse>
+            <View style={styles.collapseContainer}>
+              <TouchableOpacity
+                style={styles.collapseButtons}
+                onPress={() =>
+                  prevSortRef.current === 'created_at.desc'
+                    ? setSort('created_at.asc')
+                    : setSort('created_at.desc')
+                }>
+                {(sort === 'created_at.asc' || sort === 'created_at.desc') && (
+                  <MaterialIcons name="check" color={'#e0b422'} size={24} />
+                )}
+                <BoldText style={{marginLeft: 10}}>Created At</BoldText>
+                <View style={{flex: 1, alignItems: 'flex-end'}}>
+                  <MaterialIcons
+                    name="arrow-downward"
+                    color={'#e0b422'}
+                    size={24}
+                  />
+                </View>
+              </TouchableOpacity>
+              <Hr />
+              <TouchableOpacity
+                style={styles.collapseButtons}
+                onPress={() =>
+                  prevSortRef.current === 'release_date.desc'
+                    ? setSort('release_date.asc')
+                    : setSort('release_date.desc')
+                }>
+                {(sort === 'release_date.asc' ||
+                  sort === 'release_date.desc') && (
+                  <MaterialIcons name="check" color={'#e0b422'} size={24} />
+                )}
+                <BoldText style={{marginLeft: 10}}>Date</BoldText>
+                <View style={{flex: 1, alignItems: 'flex-end'}}>
+                  <MaterialIcons
+                    name="arrow-downward"
+                    color={'#e0b422'}
+                    size={24}
+                  />
+                </View>
+              </TouchableOpacity>
+              <Hr />
+              <TouchableOpacity
+                style={styles.collapseButtons}
+                onPress={() =>
+                  prevSortRef.current === 'title.asc'
+                    ? setSort('title.desc')
+                    : setSort('title.asc')
+                }>
+                {(sort === 'title.asc' || sort === 'title.desc') && (
+                  <MaterialIcons name="check" color={'#e0b422'} size={24} />
+                )}
+                <BoldText style={{marginLeft: 10}}>Title</BoldText>
+                <View style={{flex: 1, alignItems: 'flex-end'}}>
+                  <MaterialIcons
+                    name="arrow-downward"
+                    color={'#e0b422'}
+                    size={24}
+                  />
+                </View>
+              </TouchableOpacity>
+              <Hr />
+              <TouchableOpacity
+                style={styles.collapseButtons}
+                onPress={() =>
+                  prevSortRef.current === 'vote_average.desc'
+                    ? setSort('vote_average.asc')
+                    : setSort('vote_average.desc')
+                }>
+                {(sort === 'vote_average.asc' ||
+                  sort === 'vote_average.desc') && (
+                  <MaterialIcons name="check" color={'#e0b422'} size={24} />
+                )}
+                <BoldText style={{marginLeft: 10}}>Vote</BoldText>
+                <View style={{flex: 1, alignItems: 'flex-end'}}>
+                  <MaterialIcons
+                    name="arrow-downward"
+                    color={'#e0b422'}
+                    size={24}
+                  />
+                </View>
+              </TouchableOpacity>
+              <Hr />
+            </View>
+          </Collapse>
+          <FlatList
+            data={data}
+            style={{marginBottom: 50}}
+            // onEndReached={() => {
+            //   setPage(page + 1);
+            //   setIsExtraLoading(true);
+            // }}
+            keyExtractor={({id}) => id}
+            // ListFooterComponent={RenderFooter(isExtraLoading)}
+            renderItem={({item}) => renderItem(item)}
+          />
+        </View>
       )}
     </View>
   );
@@ -141,5 +240,15 @@ const styles = StyleSheet.create({
   itemDetails: {
     padding: 10,
     justifyContent: 'center',
+  },
+  collapseContainer: {
+    backgroundColor: '#202020',
+    marginTop: 10,
+  },
+  collapseButtons: {
+    flexDirection: 'row',
+    height: 50,
+    alignItems: 'center',
+    padding: 10,
   },
 });
